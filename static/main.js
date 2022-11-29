bca1285ebb9017a0798a5a7bde84ed7e05bc2dc3
edzis <edgars.simsons@ui.com>
2022-11-29 12:57:01 +0200

    Test the missing gap

--- static/main.js
+++ static/main.js
@@ -546,6 +546,9 @@ $(document).ready(() =>
 				3, 4, 5,
 				7, 8, 9,
 				15, 16, 17,
+
+				18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+
 				31, 32, 33,
 				47, 48, 49, 50, 51, 52, 53, 54,  // 64 concurrent WebRTC media streams is one of the common observed breaking points,
 				55, 56, 57, 58, 59, 60, 61, 62,  // so these are here to help refine the identified limit to a more accurate value
